ea9b3c002d10117e4d22d501ac93732abee6f95d
Bhavana <bhavanagadam1@gmail.com>
2025-04-03 22:26:27 +0530

    second commit

--- Day-2/Arena.py
+++ Day-2/Arena.py
@@ -1,58 +1,50 @@
 class Character:
-    def __init__(self, n, h, a, d, s):
-        self.name=n
-        self.health=h
-        self.Attack_power=a
-        self.defence=d
-        self.speed=s
-        
+    def __init__(self,name,health, attack_power, defence, speed):
+        self.name = name
+        self.health = health
+        self.attack_power = attack_power
+        self.defence = defence
+        self.speed = speed
+    
     def attack(self, target):
-        target.take_damage(self.Attack_power)
+        damage = max(1, self.attack_power - target.defence)
+        target.take_damage(self.attack_power)
+        print(f"{self.name} attacks {target.name} Deals {damage} damage")
 
     def take_damage(self, amount):
         self.health -= amount
+        print(f"{self.name} takes {amount} damage. remaining health {self.health} ")
 
-    def is_alive(self):
-        if self.health <=0:
+    def is_Alive(self):
+        if self.health > 0:
+            return True
+        else:
             return False
-        return True
+        
 
-class Warrior(Character):
-    def __init__(self, r, n, h, a, d, s):
-        super(). __init__(n, h, a, d, s)
-        self.rage = r
-        self.attack_power=a
-
-    def BerserKMode(self):
-        if rage == 10:
-            self.attack += 10
-
-    def boost_attack(self):
+class Warrior(Character):  #Here we're inherite the parent/super class(Character), child class(Warrior)
+    def __init__(self, health, attack_power, defence, speed, rage):
+        super().__init__(health, attack_power, defence, speed)
+        self.rage = rage
+    
+    def Berserk_Mode(self):
+        if self.health < 0.3:
+            self.attack_power *= 2
+            print(f"{self.name} enters Berserk Mode! Attack power doubled!")
+        
 
 class Mage(Character):
-    def __init__(self, m, n, h, a, d, s):
-        super().__init__(n, h, a, d, s)
-        self.mana = m
+    def __init__(self, health, attack_power, defence, speed, mana):
+        super().__init__(health, attack_power, defence, speed)
+        self.mana = mana
 
-    def massive_damage(self):
-        if mana 
-
+    def Fireball(self):
+        pass
 
 class Archer(Character):
-    def __init__(self, c,n, h, a, d, s):
-        super(). __init__(n, h, a, d, s)
-        self.critical_chance = c
-
+    def __init__(self, health, attack_power, defence, speed, critical_chance):
+        super().__init__(health, attack_power, defence, speed)
+        self.critical_chance = critical_chance
+    
     def Precision_Shot(self):
-
-    
-
-
-
-   
-
-
-
-
-    
-
+        pass
